6d85e2a3bee543487ef619a86c86cc18a097beb8
root <hish.b92@gmail.com>
2020-03-31 14:57:14 +0500

    Delete functionality added

--- routes.js
+++ routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 
-const data = ['leg locks','front headlocks']
+let data = ['leg locks','front headlocks']
 
 // const subData = ['toe holds','rear naked choke']
 
 module.exports = (app)=>{
 
+  //get Routes
   app.get('/',(req,res)=>{
     res.render('home',{systems:data})
   })
@@ -14,10 +15,24 @@ module.exports = (app)=>{
   app.get('/about',(req,res)=>{
     res.render('about')
   })
-
+  // post routes
   app.post('/sent',(req,res)=>{
-    console.log(req.body.item)
+
     data.push(req.body.item)
+    res.send(data)
+    console.log(data)
+  })
+
+  // delete route
+
+  app.delete('/remove/:id',(req,res)=>{
+
+   data = data.map(item=>{
+      if(item!=req.params.id){
+        return item
+      }
+    })
+    console.log("receiving",req.params.id)
     res.send(data)
   })
 
